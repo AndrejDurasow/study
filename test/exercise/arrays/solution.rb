4758bb148887c647a3460b88536faaa39bffc938
Andrej Durasow <smailik2402@gmail.com>
2020-04-24 12:55:45 +0400

    Arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,28 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |i| i > 0 ? array.max : i }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        mini = 0
+        maxi = array.length - 1
+        if array.length == 0 || array[mini] > query || array[maxi] < query
+          return -1
+        else
+          while mini <= maxi
+            pol = (mini + maxi) / 2
+            if array[pol] == query
+              return pol 
+            elsif array[pol] > query
+              maxi = pol - 1
+            elsif array[pol] < query
+              mini = pol + 1
+            else
+            end
+          end
+          return -1
+        end
       end
     end
   end
