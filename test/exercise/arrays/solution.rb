6ed7f1d6ed0a36d100cc907cb7c76c6c8f799f2d
Andrej Durasow <smailik2402@gmail.com>
2020-04-24 19:30:10 +0400

    fixed arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,24 +1,26 @@
 module Exercise
   module Arrays
     class << self
+
       def replace(array)
-        array.map { |i| i > 0 ? array.max : i }
+        max = array.reduce() { |max, n| max < n ? n : max }
+        array.map { |i| i > 0 ? max : i }
       end
 
       def search(array, query)
-        mini = 0
-        maxi = array.length - 1
-        if array.length == 0 || array[mini] > query || array[maxi] < query
+        first_el = 0
+        last_el = array.length - 1
+        if array.length == 0 || array[first_el] > query || array[last_el] < query
           return -1
         else
-          while mini <= maxi
-            pol = (mini + maxi) / 2
+          while first_el <= last_el
+            pol = (first_el + last_el) / 2
             if array[pol] == query
               return pol 
             elsif array[pol] > query
-              maxi = pol - 1
+              last_el = pol - 1
             elsif array[pol] < query
-              mini = pol + 1
+              first_el = pol + 1
             else
             end
           end
