8ff4df21eb79703776ffc341851a99b8a2179a68
Andrej Durasow <smailik2402@gmail.com>
2020-04-27 15:25:24 +0400

    rackup

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,21 @@
 module Inatra
   class << self
-    def routes
+    def routes(&block)
+      @route = {}
+      instance_eval(&block)
     end
 
     def call(env)
+      method = env['REQUEST_METHOD'].to_s.downcase
+      path = env['PATH_INFO']
+      @route[method][path].call
+    end
+
+    def method_missing(method_name, *arg, &block)
+      method = method_name.to_s
+      path = arg.first
+      @route[method] = {}
+      @route[method][path] = block
     end
   end
 end
