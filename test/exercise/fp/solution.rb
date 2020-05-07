9026377e22dd994045924f598715a98394afbb70
Andrej Durasow <smailik2402@gmail.com>
2020-05-07 14:15:48 +0400

    fixed fp now

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,15 +5,16 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        rate = array.select { |film| film['country'].to_s.include?(',') &&
-                     film["rating_kinopoisk"].present? &&
-                     film["rating_kinopoisk"].to_f > 0 }
-        result = rate.map { |film| film["rating_kinopoisk"].to_f}
+        rate = array.select { |film| !film['rating_kinopoisk'].nil? &&
+          film['country'].to_s.include?(',') &&
+          film["rating_kinopoisk"].to_f.positive?
+        }
+        result = rate.map { |film| film["rating_kinopoisk"].to_f }
         result.reduce(:+).to_f / rate.length
       end
 
       def chars_count(films, threshold)
-        res = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }              
+        res = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
         res.map { |film| film['name'].count 'и' }.reduce(:+)
       end
     end
